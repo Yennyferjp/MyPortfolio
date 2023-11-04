8692bdb54758460ed7ac3b6820c53d0381b714ed
Yennyferjp <yennyfer.jarava92@gmail.com>
2023-11-04 09:04:52 -0500

    Animación en botones con useState

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './AboutMe.module.css';
 import Yennyfer from '../../images/imagesYennyfer.png';
 import GitHub from '../../images/_github.png';
 import LinkedIn from '../../images/_linkedin.png';
 
 const AboutMe = () => {
+
+  const [githubImageScale, setGithubImageScale] = useState(1);
+  const [linkedinImageScale, setLinkedinImageScale] = useState(1);
+
+  const handleGithubMouseEnter = () => {
+    setGithubImageScale(1.1);
+  };
+
+  const handleGithubMouseLeave = () => {
+    setGithubImageScale(1);
+  };
+
+  const handleLinkedinMouseEnter = () => {
+    setLinkedinImageScale(1.1);
+  };
+
+  const handleLinkedinMouseLeave = () => {
+    setLinkedinImageScale(1);
+  };
+
   return (
     <div className={styles.AboutMe}>
       <div className={styles.leftSection}>
@@ -16,8 +36,22 @@ const AboutMe = () => {
           Soy una apasionada Desarrolladora Web con experiencia en administración de empresas. Mi viaje en el desarrollo web comenzó con el deseo de explorar un campo en constante evolución. Me dedico a crear soluciones innovadoras que mejoran la experiencia en línea de los usuarios. Con una sólida base en gestión de proyectos y una mentalidad orientada a resultados, abordo cada desafío con pasión.
           <br /><br />Mi objetivo principal es proporcionar soluciones tecnológicas de alta calidad que agreguen valor. Creo en la importancia de la colaboración y la creatividad para lograr resultados excepcionales. Estoy emocionada por el futuro en el desarrollo web y espero contribuir de manera significativa a esta comunidad en crecimiento.
           <br /><br />¡Gracias por visitar mi perfil! <span className={styles.imageWrapper}>
-            <img src={GitHub} alt='GitHub' className={styles.inlineImage} />
-             <img src={LinkedIn} alt='GitHub' className={styles.inlineImage} />
+            <img
+              src={GitHub}
+              alt='GitHub'
+              className={styles.inlineImage}
+              onMouseEnter={handleGithubMouseEnter}
+              onMouseLeave={handleGithubMouseLeave}
+              style={{ transform: `scale(${githubImageScale})` }}
+            />
+            <img
+              src={LinkedIn}
+              alt='LinkedIn'
+              className={styles.inlineImage}
+              onMouseEnter={handleLinkedinMouseEnter}
+              onMouseLeave={handleLinkedinMouseLeave}
+              style={{ transform: `scale(${linkedinImageScale})` }}
+            />
           </span>
         </p>
 
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './HomePage.module.css';
 import imageDev from '../../images/posterpixar.png';
 import Line from '../../images/Line.png';
@@ -6,22 +6,83 @@ import LinkedIn from '../../images/mdi_linkedin.png';
 import GitHub from '../../images/github.png';
 
 const HomePage = () => {
+  const [hireButtonScale, setHireButtonScale] = useState(1);
+  const [knowMoreButtonScale, setKnowMoreButtonScale] = useState(1);
+
+  const handleHireMouseEnter = () => {
+    setHireButtonScale(1.1);
+  };
+
+  const handleHireMouseLeave = () => {
+    setHireButtonScale(1);
+  };
+
+  const handleKnowMouseEnter = () => {
+    setKnowMoreButtonScale(1.1);
+  }
+
+  const handleKnowMouseLeave = () => {
+    setKnowMoreButtonScale(1);
+  }
+
+  const [githubImageScale, setGithubImageScale] = useState(1);
+  const [linkedinImageScale, setLinkedinImageScale] = useState(1);
+
+  const handleGithubMouseEnter = () => {
+    setGithubImageScale(1.1);
+  };
+
+  const handleGithubMouseLeave = () => {
+    setGithubImageScale(1);
+  };
+
+  const handleLinkedinMouseEnter = () => {
+    setLinkedinImageScale(1.1);
+  };
+
+  const handleLinkedinMouseLeave = () => {
+    setLinkedinImageScale(1);
+  };
+
   return (
     <div className={styles.homePage}>
       <div className={styles.seccionPrincipal}>
-      <img src={Line} alt="Line" className={styles.line} />
-      <img src={GitHub} alt="GitHub" className={styles.github} />
-      <img src={LinkedIn} alt="LinkedIn" className={styles.linkedin} />
-      <img src={Line} alt="Line" className={styles.line} />
+        <img src={Line} alt="Line" className={styles.line} />
+        <img
+          src={GitHub}
+          alt='GitHub'
+          className={styles.github}
+          onMouseEnter={handleGithubMouseEnter}
+          onMouseLeave={handleGithubMouseLeave}
+          style={{ transform: `scale(${githubImageScale})` }} />
+        <img src={LinkedIn} alt="LinkedIn"
+          className={styles.linkedin}
+          onMouseEnter={handleLinkedinMouseEnter}
+          onMouseLeave={handleLinkedinMouseLeave}
+          style={{ transform: `scale(${linkedinImageScale})` }}
+        />
+        <img src={Line} alt="Line" className={styles.line} />
       </div>
       <div className={styles.leftSection}>
-      <p className={styles.titulo}>Hola, Soy</p>
-      <p className={styles.subtitulo}>Yennyfer</p>
-      <p className={styles.descripcion}>
-        Desarrolladora Web | Diseñadora UX/UI  | Freelancer
-      </p>
-        <button className={styles.hireButton}>CONTRÁTAME</button>
-        <button className={styles.knowMoreButton}>SABER MÁS!</button>
+        <p className={styles.titulo}>Hola, Soy</p>
+        <p className={styles.subtitulo}>Yennyfer</p>
+        <p className={styles.descripcion}>
+          Desarrolladora Web | Diseñadora UX/UI | Freelancer
+        </p>
+        <button
+          className={styles.hireButton}
+          onMouseEnter={handleHireMouseEnter}
+          onMouseLeave={handleHireMouseLeave}
+          style={{ transform: `scale(${hireButtonScale})` }}
+        >
+          CONTRÁTAME
+        </button>
+        <button
+          className={styles.knowMoreButton}
+          onMouseEnter={handleKnowMouseEnter}
+          onMouseLeave={handleKnowMouseLeave}
+          style={{ transform: `scale(${knowMoreButtonScale})` }}
+        >SABER MÁS!</button>
       </div>
       <div className={styles.rightSection}>
         <img src={imageDev} alt="Tu Imagen" />
